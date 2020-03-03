b06b691c8d11c8082e9dfab0067eb8b6a77fb374
diva maretta <54298696+divamaretta@users.noreply.github.com>
2020-03-03 20:46:37 +0700

    Add files via upload

---
+++ Latihan1.php
@@ -0,0 +1,5 @@
+<?php
+   echo "Namaku Diva Maretta";
+   echo "<br>";
+   echo "Aku sedang belajar PHP di SMK Telkom Malang"
+   ?>
---
+++ Latihan4.php
@@ -0,0 +1,8 @@
+<?php
+$nama = "Diva Maretta";
+function tampil_nama() {
+global $nama;
+echo "Nama Saya : " .$nama;
+}
+tampil_nama();
+?>
